dba42fbf0957bfe3df6e6c06b7e5a4f7341a6e85
futoase <futoase@gmail.com>
2013-05-30 17:26:49 +0900

    Modified: Update version, README.md.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
   name="PyQCheck",
-  version="0.1",
+  version="0.2",
   description="PyQCheck is Quick Check-like random testing framework.",
   author="Keiji Matsuzaki",
   author_email="futoase@gmail.com",
